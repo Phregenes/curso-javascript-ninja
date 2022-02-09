0f1efc0529da6cb93b52166ec12f6946185fa1ba
phregenes <phregenes@gmail.com>
2022-02-09 01:37:53 -0300

    add challenge 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -4,15 +4,23 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 */
 // ?
 
+var array = [1, 2, 3, 4, 5];
+
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 // ?
 
+funcArray = (array) => {
+    return array;
+}
+
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
 // ?
+
+array[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -22,16 +30,24 @@ segundo parâmetro.
 */
 // ?
 
+funcParm = ( x, y ) => {
+    return x[y];
+}
+
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
 // ?
+
+var arrayX = [1, 'a', true, null, undefined];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
 // ?
+
+arrayX;
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -49,10 +65,26 @@ os livros.
 */
 // ?
 
+book = (nome) => {
+    var livro = {
+        nome: nome,
+        quantidadePaginas: 100,
+        autor: 'Pedro',
+        editora: 'Editora'
+    }
+    if(nome) {
+        return livro;
+    } else {
+        return livro;
+    }
+}
+
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
 // ?
+
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
@@ -61,6 +93,8 @@ usando a frase:
 */
 // ?
 
+book().quantidadePaginas;
+
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
@@ -68,9 +102,13 @@ a frase:
 */
 // ?
 
+book().autor;
+
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
 // ?
+
+book().editora;
